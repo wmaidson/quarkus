c9e995a9e1f4ce0d3f2ab668f1dcde4ab9f217bc
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-20 21:10:23 -0300

    create service

--- src/main/java/br/com/bitcoin/service/BitcoinService.java
+++ src/main/java/br/com/bitcoin/service/BitcoinService.java
@@ -1,4 +1,20 @@
 package br.com.bitcoin.service;
 
-public class BitcoinService {
+import br.com.bitcoin.model.Bitcoin;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/bitcoins")
+@RegisterRestClient
+public interface BitcoinService {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Bitcoin> listar();
 }
