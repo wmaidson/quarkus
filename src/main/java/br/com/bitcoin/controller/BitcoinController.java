4cbba773a8a6bad91f97feb7fbd0d249c4e876be
wagner maidson <wagner.rmaidson@gmail.com>
2021-11-20 22:30:56 -0300

    create controller

--- src/main/java/br/com/bitcoin/controller/BitcoinController.java
+++ src/main/java/br/com/bitcoin/controller/BitcoinController.java
@@ -1,2 +1,26 @@
-package br.com.bitcoin.service;public class BitcoinController {
+package br.com.bitcoin.controller;
+
+import br.com.bitcoin.model.Bitcoin;
+import br.com.bitcoin.service.BitcoinService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("bitcoins")
+public class BitcoinController {
+
+    @Inject
+    @RestClient
+    BitcoinService bitcoinService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Bitcoin> listar() {
+        return bitcoinService.listar();
+    }
 }
